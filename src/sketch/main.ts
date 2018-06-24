e0d63cd19ff92eb3daabbccaa898ebc728224be2
Rupertofly <rupertqmoore@me.com>
2018-06-24 15:58:44 +1000

    🎉 init

---
+++ src/sketch/main.ts
@@ -0,0 +1,8 @@
+// @ts-ignore
+// @ts-ignore
+
+export function setup() {}
+
+export function draw() {}
+
+export function mousePressed() {}
---
+++ typings/global.d.ts
@@ -0,0 +1,10 @@
+declare class CCapture {
+  constructor({});
+  start();
+}
+declare const fBufffer: p5.Graphics;
+declare const bBufffer: p5.Graphics;
+
+declare namespace dat {
+
+}
