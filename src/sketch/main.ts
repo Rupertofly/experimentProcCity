c79c194175656d09969c0341da1ca9f4b4dea229
Rupertofly <Rupertofly@users.noreply.github.com>
2018-07-07 17:02:47 +1000

    someChanges

--- src/sketch/main.ts
+++ src/sketch/main.ts
@@ -4,7 +4,7 @@ import * as _ from 'lodash';
 import LCell from './classes/LCell';
 import CellTypes from './enums';
 // import Offset from 'polygon-offset';
-import getC from './lib/pallete';
+import { getC } from './lib/pallete';
 
 let vFunc;
 let vDiag: VoronoiDiagram<LCell>;
@@ -13,10 +13,14 @@ const framesSinceSitesChanged = 1000;
 let c: p5.Renderer;
 
 export function setup() {
-  createCanvas( window.innerHeight, window.innerHeight );
-  background( getC( 3, 5 ).hex );
+  c = createCanvas( window.innerWidth - 20, window.innerWidth );
+
+  c.parent( '#canvas' );
+  background( getC( 2, 5 ).hex );
 }
 
-export function draw() {}
+export function draw() {
+  background( getC( 1, 6 ).hex );
+}
 
 export function mousePressed() {}
